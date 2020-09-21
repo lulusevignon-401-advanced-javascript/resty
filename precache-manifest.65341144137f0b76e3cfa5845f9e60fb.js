5771912a0606d3dece058c4ed583da06eb2e30f9
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-09-20 20:51:41 -0700

    Updates

---
+++ precache-manifest.65341144137f0b76e3cfa5845f9e60fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae72509b732fad5b462f465cc43e456",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "81842261103ce8b0ddd5",
+    "url": "/resty/static/css/main.ecb59fd0.chunk.css"
+  },
+  {
+    "revision": "42647517bbfb896517a0",
+    "url": "/resty/static/js/2.e1f8de1e.chunk.js"
+  },
+  {
+    "revision": "28fa8ae9817b1f7a5dea7108e26b4348",
+    "url": "/resty/static/js/2.e1f8de1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81842261103ce8b0ddd5",
+    "url": "/resty/static/js/main.a4e1ebe4.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  },
+  {
+    "revision": "33ccf91b58b29eaa73e2f8ed618bf721",
+    "url": "/resty/static/media/giphyLoad.33ccf91b.gif"
+  }
+]);
